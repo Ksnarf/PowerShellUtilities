9b4f73e85e766fce6acbd189ba8f58404c14331b
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2018-12-12 14:17:45 -0800

    Create GetOSConsoleView.ps1
    Replace remotehost with the machine you are looking to pull data for and it will show in the console the OS type, 32bit or x64 as well as the latest major service pack for the machine

---
+++ GetOSConsoleView.ps1
@@ -0,0 +1,8 @@
+$Server = "remotehost" 
+$OS = Get-WMIObject -class Win32_OperatingSystem -ComputerName $Server
+
+ Write-Host "OS Version =" $sProperty.Caption
+ Write-Host "OS Architecture ="$sProperty.OSArchitecture
+ Write-Host "Service Pack Version ="$sProperty.ServicePackMajorVersion
+ 
+ 
