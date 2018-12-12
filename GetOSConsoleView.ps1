e5d7d86651a675dc4f5477b811d4e48d3fefd5dc
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2018-12-12 15:13:49 -0800

    Update GetOSConsoleView.ps1

--- GetOSConsoleView.ps1
+++ GetOSConsoleView.ps1
@@ -1,9 +1,10 @@
 Import-Module ActiveDirectory
+Function OSVersion {
 $Server = "remotehost" 
 $OS = Get-WMIObject -class Win32_OperatingSystem -ComputerName $Server
 
  Write-Host "OS Version =" $sProperty.Caption
  Write-Host "OS Architecture ="$sProperty.OSArchitecture
  Write-Host "Service Pack Version ="$sProperty.ServicePackMajorVersion
- 
+}
  
